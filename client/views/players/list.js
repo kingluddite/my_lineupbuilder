713da110966e68b3ef4603af31a4753535f1aea2
pip_lineupbuilder <howley.phil@gmail.com>
2015-09-20 01:30:38 -0700

    modularize team roster page and not logged in page, clean home

---
+++ client/views/players/list.js
@@ -0,0 +1,48 @@
+// when the trashcan icon is clicked, the player is deleted
+var removePlayer = function() {
+  Players.remove({
+    _id: Session.get('sPlayerId')
+  });
+};
+
+Template.tTeamRoster.helpers({
+  // grab all the players and provide collection for roster template
+  cPlayers: function() {
+    // only if the user is logged in
+    if (Meteor.user()) {
+      // grab all the players the user created (so we know it's their
+      //   team)
+
+      return Players.find({
+        createdBy: Meteor.user()._id
+      });
+    } else {
+      this.ready();
+    }
+  }
+});
+// coach completes his roster and lets us know
+//  visually we just add a checkbox, muted text and a line-through
+Template.tHome.events({
+  'click .roster-complete': function(evt, tmpl) {
+    if (evt.target.checked) {
+      Session.setPersistent("sRosterComplete", true);
+    } else {
+      Session.setPersistent("sRosterComplete", false);
+    }
+  },
+  'click .remove': function(evt, tmpl) {
+    evt.preventDefault();
+
+    var playerCount = Players.find().count();
+
+    if (confirm("Delete this player?")) {
+      Session.set('sPlayerId', this._id);
+      removePlayer();
+      Session.set('sPlayerId', null);
+    }
+    if (playerCount <= 26) {
+      $("#teamRoster").show();
+    }
+  }
+});
