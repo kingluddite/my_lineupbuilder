e47118e9495fd3f0d7d49468c6ac2c6eb2da2e26
pip_lineupbuilder <howley.phil@gmail.com>
2015-09-21 11:19:36 -0700

    team, game progress box update works

--- client/views/progress/list.js
+++ client/views/progress/list.js
@@ -1,31 +1,12 @@
 Template.ProgressList.helpers({
-  teamCreatedStatus: function() {
-    if (!Session.get('sGameCreated')) {
-      $('.team-created').html('<span> Team Created</span>');
-      $('.team-created span').css('text-decoration', 'none');
-      $('.team-created').removeClass('text-muted');
-      return Session.get('sTeamCreated');
-    } else {
-      $('.team-created').html('<i class="fa fa-check"></i> <span> Team Created</span>');
-      $('.team-created span').css('text-decoration', 'line-through');
-      $('.team-created').addClass('text-muted');
-    }
-  },
   teamId: function() {
     return Session.get('sTeamId');
   },
-  gameCreatedStatus: function() {
-    if (!Session.get('sGameCreated')) {
-      $('.game-created').html('<span> game Created</span>');
-      $('.game-created span').css('text-decoration', 'none');
-      $('.game-created').removeClass('text-muted');
-      return Session.get('sGameCreated');
-    } else {
-      $('.game-created').html('<i class="fa fa-check"></i> <span> Game Created</span>');
-      $('.game-created span').css('text-decoration', 'line-through');
-      $('.game-created').addClass('text-muted');
-    }
+  gameId: function() {
+    return Session.get('sGameId');
   },
+  // when Roster completed checkbox checked the follow styles
+  //  get added to the roster completed section of the progress box
   rosterCreatedStatus: function() {
     if (!Session.get('sRosterComplete')) {
       $('.roster-created').html('<span> Roster Created</span>');
@@ -38,4 +19,5 @@ Template.ProgressList.helpers({
       $('.roster-created').addClass('text-muted');
     }
   }
+
 });
