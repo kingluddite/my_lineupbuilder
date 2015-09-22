a3e6f543e62976d4ae11a434a943fe065f7a4bfd
pip_lineupbuilder <howley.phil@gmail.com>
2015-09-21 21:45:42 -0700

    open close all panels added

--- client/main.js
+++ client/main.js
@@ -9,22 +9,6 @@ Session.setDefaultPersistent("sTeamCreated", false);
 Session.setDefaultPersistent("sGameCreated", false);
 Session.setDefaultPersistent("sRosterComplete", false);
 
-// jQuery(function($) {
-//   $('.panel-heading span.clickable').on("click", function(e) {
-//     if ($(this).hasClass('panel-collapsed')) {
-//       // expand the panel
-//       $(this).parents('.panel').find('.panel-body').slideDown();
-//       $(this).removeClass('panel-collapsed');
-//       $(this).find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-//     } else {
-//       // collapse the panel
-//       $(this).parents('.panel').find('.panel-body').slideUp();
-//       $(this).addClass('panel-collapsed');
-//       $(this).find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-//     }
-//   });
-// });
-
 Template.body.events({
   'click .panel-heading span.clickable': function(evt) {
     // console.log('works');
