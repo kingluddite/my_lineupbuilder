315ecae889a687bd135a66f19e04a276596c1806
pip_lineupbuilder <howley.phil@gmail.com>
2015-09-27 20:30:03 -0700

    add moment and global date and time helpers

--- client/config.js
+++ client/config.js
@@ -1,1 +1,21 @@
+// Accounts.ui.config({
+//   passwordSignupFields: 'USERNAME_ONLY'
+// });
 
+
+// allows you to easily format JavaScript dates
+UI.registerHelper('formatDate', function(datetime) {
+  if (moment) {
+    return moment(datetime).format('MMMM Do YYYY');
+  } else {
+    return datetime;
+  }
+});
+
+UI.registerHelper('formatTime', function(datetime) {
+  if (moment) {
+    return moment(datetime).format('h:mm a');
+  } else {
+    return datetime;
+  }
+});
