bfa03a8d7443481e766fc2316c969e47e11eb123
pip_lineupbuilder <howley.phil@gmail.com>
2015-09-29 12:33:06 -0700

    clear persistent sessions when logging out

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -2,7 +2,34 @@ Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY'
 });
 
+
+
+// Accounts.onLogout(function() {
+//   console.log('yo');
+//   Session.clearPersistent();
+// });
+
+
 accountsUIBootstrap3.logoutCallback = function(error) {
   if (error) console.log("Error:" + error);
+  Session.clearPersistent();
   Router.go('Home');
 }
+
+
+// allows you to easily format JavaScript dates
+UI.registerHelper('formatDate', function(datetime) {
+  if (moment) {
+    return moment(datetime).format('MMMM Do YYYY');
+  } else {
+    return datetime;
+  }
+});
+
+UI.registerHelper('formatTime', function(datetime) {
+  if (moment) {
+    return moment(datetime).format('h:mm a');
+  } else {
+    return datetime;
+  }
+});
