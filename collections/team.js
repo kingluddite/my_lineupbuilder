197e3d58e0f2a305bc4c89562d08b9b245776e7f
Philip Howley <howley.phil@gmail.com>
2015-09-30 10:25:11 -0700

    update laptop and fix validateTeam undefined issue

--- collections/team.js
+++ collections/team.js
@@ -1,1 +1,10 @@
 Teams = new Meteor.Collection('teams');
+
+validateTeam = function(team) {
+    var errors = {};
+    // if (!post.title)
+    //   errors.title = "Please fill in a headline";
+    if (!team.logoUrl)
+        errors.url = "Please fill in a URL";
+    return errors;
+}
