73ab7ead8a865a8b7080ade6a268a3afddd106b8
pip_lineupbuilder <howley.phil@gmail.com>
2015-10-08 13:06:48 -0700

    alter email verification template

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,16 @@
 // add colon at end and add port number 587
 
 process.env.MAIL_URL = 'smtp://postmaster%40sandboxcae008ae47c34078926740dcf3046605.mailgun.org:494l9rso5u63@smtp.mailgun.org:587';
+Accounts.emailTemplates.from = 'no-reply@mysoccerlineupbuilder.com';
+Accounts.emailTemplates.siteName = 'My Soccer Lineup Builder';
+
+Accounts.emailTemplates.verifyEmail.subject = function(user) {
+  return 'Confirm Your Email Address';
+};
+Accounts.emailTemplates.verifyEmail.text = function(user, url) {
+  return 'Click on the following link to verify your email address: ' + url;
+};
+
 Accounts.config({
   sendVerificationEmail: true
 });
-
