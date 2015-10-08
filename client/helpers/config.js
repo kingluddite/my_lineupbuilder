c9ff437184a23b345434e8f4172b0efff3a9c675
pip_lineupbuilder <howley.phil@gmail.com>
2015-10-08 12:43:10 -0700

    fix logout to team instead of home

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,5 +1,5 @@
 Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_ONLY'
+  passwordSignupFields: 'USERNAME_ONLY'
 });
 
 
@@ -11,42 +11,42 @@ Accounts.ui.config({
 
 
 accountsUIBootstrap3.logoutCallback = function(error) {
-    if (error) console.log("Error:" + error);
-    Session.clearPersistent();
-    // clear these sessions out when use logs out
-    Session.clear('sTeamId');
-    Session.clear('sGameId');
-    Session.clear('sSeasonId');
-    Session.clear('sLeagueId');
-    Session.clear('sFormationChosen');
-    Session.clear('sPositionsNamed');
-    Session.clear('sRosterComplete');
-    Session.clear('sRosterCreated');
-    Session.clear('sTeamNew');
-    Session.clear('sGameNew');
-    Session.clear('sLeagueNew');
-    Session.clear('sSeasonNew');
-    Session.clear('sAddTeam');
-    Session.clear('sAddGame');
-    Session.clear('sAddLeague');
-    Session.clear('sAddSeason');
-    Router.go('Home');
+  if (error) console.log("Error:" + error);
+  Session.clearPersistent();
+  // clear these sessions out when use logs out
+  Session.clear('sTeamId');
+  Session.clear('sGameId');
+  Session.clear('sSeasonId');
+  Session.clear('sLeagueId');
+  Session.clear('sFormationChosen');
+  Session.clear('sPositionsNamed');
+  Session.clear('sRosterComplete');
+  Session.clear('sRosterCreated');
+  Session.clear('sTeamNew');
+  Session.clear('sGameNew');
+  Session.clear('sLeagueNew');
+  Session.clear('sSeasonNew');
+  Session.clear('sAddTeam');
+  Session.clear('sAddGame');
+  Session.clear('sAddLeague');
+  Session.clear('sAddSeason');
+  Router.go('TeamList');
 }
 
 
 // allows you to easily format JavaScript dates
 UI.registerHelper('formatDate', function(datetime) {
-    if (moment) {
-        return moment(datetime).format('MMMM Do YYYY');
-    } else {
-        return datetime;
-    }
+  if (moment) {
+    return moment(datetime).format('MMMM Do YYYY');
+  } else {
+    return datetime;
+  }
 });
 
 UI.registerHelper('formatTime', function(datetime) {
-    if (moment) {
-        return moment(datetime).format('h:mm a');
-    } else {
-        return datetime;
-    }
+  if (moment) {
+    return moment(datetime).format('h:mm a');
+  } else {
+    return datetime;
+  }
 });
