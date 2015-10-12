7970dbbbdf21047b510ab374678fb681b45dd2e5
pip_lineupbuilder <howley.phil@gmail.com>
2015-10-12 15:15:30 -0700

    home page layout responsive

--- server/publications/teams.js
+++ server/publications/teams.js
@@ -1,13 +1,13 @@
 Meteor.publish('current-team', function() {
-  user = Meteor.users.findOne({
-    _id: this.userId
-  });
+  user = Meteor.users.findOne(this.userId);
   if (user) {
     // if (user.emails[0].verified) {
     return Teams.find({
       createdBy: this.userId
     });
     // }
+  } else {
+    return;
   }
 
 });
