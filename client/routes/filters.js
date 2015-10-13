fafbbc03d82035ae7f9a2c50994d88223e0f9d11
pip_lineupbuilder <howley.phil@gmail.com>
2015-10-13 11:04:36 -0700

    add route filters for logging in

---
+++ client/routes/filters.js
@@ -0,0 +1,90 @@
+/*
+  Route Filters
+  Filters for managing user access to application routes.
+ */
+
+/*
+  Filter: Check if a User is Logged In
+  If a user is not logged in and attempts to go to an authenticated route,
+  re-route them to the index/beta signup screen.
+ */
+var checkUserLoggedIn, 
+    userAuthenticatedAdmin, 
+    userAuthenticatedBetaTester;
+
+checkUserLoggedIn = function ( ) {
+  if ( !Meteor.loggingIn( ) && !Meteor.user( ) ) {
+    return Router.go( '/' );
+  } else {
+    return this.next( );
+  }
+};
+
+/*
+  Filter: Check if a Beta Tester User Exists
+  If a user is logged in and attempts to go to a public route, re-route
+  them to the main dashboard screen.
+ */
+
+userAuthenticatedBetaTester = function ( ) {
+  var isBetaTester, loggedInUser;
+  loggedInUser = Meteor.user( );
+  isBetaTester = Roles.userIsInRole( loggedInUser, [ 'tester' ] );
+  if ( !Meteor.loggingIn( ) && isBetaTester ) {
+    return Router.go( '/dashboard' );
+  } else {
+    return this.next( );
+  }
+};
+
+/*
+  Filter: Check if an Admin User Exists
+  If a user is logged in and attempts to go to a public route, re-route
+  them to the main invites screen.
+ */
+
+userAuthenticatedAdmin = function ( ) {
+  var isAdmin, loggedInUser;
+  loggedInUser = Meteor.user( );
+  isAdmin      = Roles.userIsInRole( loggedInUser, [ 'admin' ] );
+  if ( !Meteor.loggingIn( ) && isAdmin ) {
+    return Router.go( '/invites' );
+  } else {
+    return this.next( );
+  }
+};
+
+Router.onBeforeAction( checkUserLoggedIn, {
+  except: [
+    'index',
+    'signup',
+    'signup/:token',
+    'login',
+    'recover-password',
+    'reset-password'
+  ]
+} );
+
+
+Router.onBeforeAction( userAuthenticatedBetaTester, {
+  only: [ 
+    'index', 
+    'signup', 
+    'signup/:token', 
+    'login', 
+    'recover-password', 
+    'reset-password', 
+    'invites' 
+  ]
+} );
+
+Router.onBeforeAction( userAuthenticatedAdmin, {
+  only: [ 
+    'index', 
+    'signup', 
+    'signup/:token', 
+    'login', 
+    'recover-password', 
+    'reset-password' 
+  ]
+} );
