07cb94b6b095be26a5195e47bd7c0d6436b0f8fe
pip_lineupbuilder <howley.phil@gmail.com>
2015-10-12 21:45:29 -0700

    add allow deny rules for team client

--- collections/team.js
+++ collections/team.js
@@ -8,3 +8,39 @@ validateTeam = function(team) {
     errors.name = "Please enter a Team Name";
   return errors;
 }
+aaa
+/*=============================
+=            Allow            =
+=============================*/
+Teams.allow({
+  insert: function() {
+    // Disallow inserts on the client by default
+    return false;
+  },
+  update: function() {
+    // Disallow updates on the client by default
+    return false;
+  },
+  remove: function() {
+    // Disallow removes on the client by default
+    return false;
+  }
+});
+
+/*============================
+=            Deny            =
+============================*/
+Teams.deny({
+  insert: function() {
+    // Deny inserts on the client by default
+    return true;
+  },
+  update: function() {
+    // Deny updates on the client by default
+    return true;
+  },
+  remove: function() {
+    // Deny removes on the client by default
+    return true;
+  }
+});
