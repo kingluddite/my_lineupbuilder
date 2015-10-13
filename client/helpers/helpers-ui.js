8b852b05a6b7d7d8a3280d7bfc1e123af6023c87
pip_lineupbuilder <howley.phil@gmail.com>
2015-10-13 13:03:00 -0700

    add useful comment for helpers

--- client/helpers/helpers-ui.js
+++ client/helpers/helpers-ui.js
@@ -1,3 +1,9 @@
+/*==================================
+=            UI Helpers            =
+==================================*/
+/* helpers defined here can be reused all throughout our application!
+*/
+
 /*
 * using moment library
 * we display a date for each invite
@@ -5,6 +11,11 @@
 * this helper takes a unix timestamp and splits it out into a human
 *  readable string
 * example: 1415983049 becomes Friday, November 14th, 2014
+* we divide be 1000 because our db date is in milliseconds and
+*  moment expect it to be in seconds
+*/
+/* use with
+* {{epochToString <variable here>}}
 */
 UI.registerHelper('epochToString', function(timestamp) {
   return moment.unix(timestamp / 1000).format("MMMM Do, YYYY");
