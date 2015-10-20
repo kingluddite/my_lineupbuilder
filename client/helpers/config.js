bfc38ac1484649f96cec615889018c5c5bfdda13
pip_lineupbuilder <howley.phil@gmail.com>
2015-10-20 13:39:33 -0700

    add working logout that clears sessions and redirects to index (home) page

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -5,13 +5,7 @@
 
 
 
-// Accounts.onLogout(function() {
-//   console.log('yo');
-//   Session.clearPersistent();
-// });
-
-
-accountsUIBootstrap3.logoutCallback = function(error) {
+Meteor.logout(function(error) {
   if (error) console.log("Error:" + error);
   Session.clearPersistent();
   // clear these sessions out when use logs out
@@ -31,8 +25,29 @@ accountsUIBootstrap3.logoutCallback = function(error) {
   Session.clear('sAddGame');
   Session.clear('sAddLeague');
   Session.clear('sAddSeason');
-  Router.go('Home');
-}
+  Router.go('Index');
+});
 
 
-
+//accountsUIBootstrap3.logoutCallback = function(error) {
+// if (error) console.log("Error:" + error);
+// Session.clearPersistent();
+// // clear these sessions out when use logs out
+// Session.clear('sTeamId');
+// Session.clear('sGameId');
+// Session.clear('sSeasonId');
+// Session.clear('sLeagueId');
+// Session.clear('sFormationChosen');
+// Session.clear('sPositionsNamed');
+// Session.clear('sRosterComplete');
+// Session.clear('sRosterCreated');
+// Session.clear('sTeamNew');
+// Session.clear('sGameNew');
+// Session.clear('sLeagueNew');
+// Session.clear('sSeasonNew');
+// Session.clear('sAddTeam');
+// Session.clear('sAddGame');
+// Session.clear('sAddLeague');
+// Session.clear('sAddSeason');
+// Router.go('Home');
+//}
