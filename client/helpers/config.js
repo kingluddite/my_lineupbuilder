afc0da2c4bf3f9c07a3bfd0f01fded67996e6a17
pip_lineupbuilder <howley.phil@gmail.com>
2015-10-20 13:42:49 -0700

    removed bootstrap 3 login package, code and add comment for manual logout meteor function

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -2,9 +2,7 @@
 //   passwordSignupFields: 'USERNAME_ONLY'
 // });
 
-
-
-
+/* we want to log out, clear all sessions and return user to home page */
 Meteor.logout(function(error) {
   if (error) console.log("Error:" + error);
   Session.clearPersistent();
@@ -27,27 +25,3 @@ Meteor.logout(function(error) {
   Session.clear('sAddSeason');
   Router.go('Index');
 });
-
-
-//accountsUIBootstrap3.logoutCallback = function(error) {
-// if (error) console.log("Error:" + error);
-// Session.clearPersistent();
-// // clear these sessions out when use logs out
-// Session.clear('sTeamId');
-// Session.clear('sGameId');
-// Session.clear('sSeasonId');
-// Session.clear('sLeagueId');
-// Session.clear('sFormationChosen');
-// Session.clear('sPositionsNamed');
-// Session.clear('sRosterComplete');
-// Session.clear('sRosterCreated');
-// Session.clear('sTeamNew');
-// Session.clear('sGameNew');
-// Session.clear('sLeagueNew');
-// Session.clear('sSeasonNew');
-// Session.clear('sAddTeam');
-// Session.clear('sAddGame');
-// Session.clear('sAddLeague');
-// Session.clear('sAddSeason');
-// Router.go('Home');
-//}
