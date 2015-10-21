4863c02b6b9bc8f0ad49bf522a1fff8e8078d064
pip_lineupbuilder <howley.phil@gmail.com>
2015-10-21 00:53:50 -0700

    update breadcrumbs

--- client/includes/_breadcrumbs.js
+++ client/includes/_breadcrumbs.js
@@ -1,5 +1,17 @@
 Template.Breadcrumbs.helpers({
+  sTeamId: function () {
+    return Session.get('sTeamId');
+  },
   sLeagueId: function () {
     return Session.get('sLeagueId');
+  },
+  sSeasonId: function() {
+    return Session.get('sSeasonId');
+  },
+  sRosterComplete: function() {
+    return Session.get('sRosterComplete');
+  },
+  sGameId: function() {
+    return Session.get('sGameId');
   }
 });
